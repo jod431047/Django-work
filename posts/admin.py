5eb9ad419ecb65653996afd2e4a1e48bef02b3b1
jod431047 <jod431047@gmail.com>
2023-06-21 14:15:59 +0200

    django new file

---
+++ posts/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Post, Author
+admin.site.register(Post)   # post in admin seite anmelden
+admin.site.register(Author) 
---
+++ posts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+# Create your models here, post erstellen, bd designing, bei eine zugefügte 
+# in models muss folgendes gemacht werden: python manage.py makemigrations ; python manage.py migrate
+# ; python manage.py runserver
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+    def __str__(self):                                # um in adminpanel den name zu ändern
+        return self.name
+    
+
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    Content = models.TextField(max_length=15000)
+    author = models.ForeignKey(Author,related_name='post_Author',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+   
